f669fb6523f735214bcb6f25c85e797a54432d10
Renjie Zhu <johnsonlumia@live.com>
2016-04-24 15:22:05 -0700

    initial commit

---
+++ __calc__.py
@@ -0,0 +1,10 @@
+# They may serve as a calculating modules for my Evenpay
+# For now it is only a test for several python code
+
+def main():
+    name = input("What is your name? ")
+    text = ("Hello, %s." % (name))
+    print(text)
+
+if __name__ == '__main__':
+    main()
